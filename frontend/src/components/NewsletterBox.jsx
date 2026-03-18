feaf6d6a73b83f322b7d8265f73343920406efa5
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-03-18 23:12:52 +0530

    newslater

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -1,23 +1,43 @@
 import React from 'react'
+import { toast } from 'react-toastify';
 
-const NewsletterBox = () => {
+const ContactUs = () => {
+    const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    formData.append("access_key", "ab4d4b2a-dfef-449e-9c3f-35f3e8004057");
 
-    const onSubmitHandler = (event) => {
-        event.preventDefault();
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+    if(data.success){
+    toast.success('Thank You For Your Customization !')
+    event.target.reset();
+    }else{
+    toast.error(data.message)
     }
+        
+    } catch (error) {
+        toast.error(error.message)
+    }
+  };
 
   return (
     <div className=' text-center'>
-      <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
+      <p className='text-2xl font-medium text-gray-800'>Customize Your Perfect Toy</p>
       <p className='text-gray-400 mt-3'>
-      Subscribe now and enjoy an instant 20% discount on your first order—don’t miss the fun! 
+      Customize every detail to create a unique product experience designed just for you.
       </p>
-      <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
-        <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' required/>
-        <button type='submit' className='bg-black text-white text-xs px-10 py-4'>SUBSCRIBE</button>
+      <form onSubmit={onSubmit} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6  pl-3'>
+        <textarea rows={6}  name='message' placeholder='Enter here which type of toys you want' className='w-full p-3 text-sm outline-none rounded-lg border border-gray-300 dark:border-gray-600'/>
+        <button type='submit' className='bg-black text-white text-xs px-10 py-4'>Submit</button>
       </form>
     </div>
   )
 }
 
-export default NewsletterBox
+export default ContactUs
